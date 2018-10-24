44f72fcf8507fcf04342b9fd4becaf64f74c4be5
Charley Ferrari <charley@charleyferrari.com>
2018-10-24 16:27:43 -0400

    new flask API

--- module4.5/toy-rest-api.py
+++ module4.5/toy-rest-api.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -17,6 +18,17 @@ def return_hello():
 @app.route('/complex/<string:word>')
 def return_complex(word):
     return jsonify({'complex': word})
+
+# And this is an API meant to serve some housing price index data
+
+
+@app.route('/hpi/<strong:seasonality>/<string:metro>')
+def return_hpi_data(seasonality, metro):
+    raw_data = pd.read_csv(
+        'https://raw.githubusercontent.com/charleyferrari/CUNY_DATA608/master/lecture3/Sample%20Code/hpi.csv')
+    filtered_data = raw_data.loc[['DATE', 'Tier', 'HPI'],
+                                 raw_data['Seasonality'] == seasonality & raw_data['metro'] == metro]
+    filtered_data.sort_values('')
 
 
 if __name__ == '__main__':
